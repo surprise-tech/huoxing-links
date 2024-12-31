097b18c2c6fbb48d46c5c2463c421de65c68a114
atshike <atshike@foxmail.com>
2024-12-31 15:14:20 +0800

    fix:配置

--- serve/app/Http/Controllers/Api/ConfigController.php
+++ serve/app/Http/Controllers/Api/ConfigController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Forms\BaseConfig;
-use App\Forms\PaymentSet;
-use App\Forms\VerifyCode;
 use App\Forms\WebSite;
 use Ugly\Base\Http\Controllers\SimpleFormController;
 
@@ -13,7 +11,5 @@ class ConfigController extends SimpleFormController
     protected array $form = [
         'base' => BaseConfig::class,
         'web_site' => WebSite::class,
-        'payment' => PaymentSet::class,
-        'verify_code' => VerifyCode::class,
     ];
 }
