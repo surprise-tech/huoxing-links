e946b230f3f825d515796c2d1becb060487c6f0b
atshike <atshike@foxmail.com>
2025-01-03 17:45:26 +0800

    fix：admin

--- admin/dist/assets/create-or-look-048706a1.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./create-or-look.vue_vue_type_script_setup_true_lang-46ba9753.js";import"./form-e5ba5cf1.js";import"./.pnpm-9b0f0a98.js";import"./index-501ba653.js";import"./app-af90cb42.js";export{o as default};
